0fea03df3df2a4d2a2f783c8cb310add5a38b5c7
Mateo <mateopinzon19@gmail.com>
2025-09-14 18:32:05 -0500

    Debug tipos de datos

---
+++ Conceptos/Debug-Challenge/03.TiposDatos.js
@@ -0,0 +1,20 @@
+
+/* const numero1 = "10";
+const numero2 = 5;
+const resultado = numero1 + numero2;
+
+console.log(resultado); */ // ¿Por qué no da 15?
+
+// 🔍 ERRORES ENCONTRADOS:
+// 1. Línea 1: 
+
+// ✅ CÓDIGO CORREGIDO:
+// (Reescribe el código sin errores)
+const numero1 = 10;
+const numero2 = 5;
+const resultado = numero1 + numero2;
+console.log(resultado); // 15
+
+// 💡 EXPLICACIÓN:
+// ¿Por qué ocurrían estos errores?
+// porque la variable numero1 el tipo de dato estaba como string y no se puede sumar
