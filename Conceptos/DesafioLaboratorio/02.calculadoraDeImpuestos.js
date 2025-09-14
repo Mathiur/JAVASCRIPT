905334314c830a74529b9add955eccc33df43093
Mateo <mateopinzon19@gmail.com>
2025-09-13 23:49:41 -0500

    eliminar carpeta Debug y Desafio

--- Conceptos/DesafioLaboratorio/02.calculadoraDeImpuestos.js
+++
@@ -1,7 +0,0 @@
-const IVA = 0.19;
-let precioBruto = 100;
-let impuesto = (precioBruto * IVA);
-let precioNeto = (precioBruto + impuesto);
-// 🧪 CASOS DE PRUEBA:
-console.log(impuesto);
-console.log(precioNeto); 
