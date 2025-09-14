ad0d80b03bf1b9c79876b5ab377a16a0229c20a7
Mateo <mateopinzon19@gmail.com>
2025-09-13 23:20:58 -0500

    Ejercicios Debug

---
+++ Conceptos/Debug_Challenge/Variables.js
@@ -0,0 +1,24 @@
+// 🚨 Este código tiene errores 
+/* const nombreCompleto = 'Ana García';
+let edad = 25;
+const PI = 3.14159;
+
+// Quiero cambiar la edad y el nombre
+edad = 26;
+nombreCompleto = 'Ana G.' */
+
+
+// ✅ CÓDIGO CORREGIDO:
+let nombreCompleto = 'Ana García';
+let edad = 25;
+const PI = 3.14159;
+
+// Quiero cambiar la edad y el nombre
+edad = 26;
+nombreCompleto = 'Ana G.'
+
+console.log(edad);
+console.log(nombreCompleto);
+// 💡 EXPLICACIÓN:
+// ¿Por qué ocurrían estos errores?
+// la variable const no se pude volver a inicializar
