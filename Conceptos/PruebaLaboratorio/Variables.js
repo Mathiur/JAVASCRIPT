63365bf99806a9c166ac3679fe5c8ea46b4c0d1e
Mateo <mateopinzon19@gmail.com>
2025-09-13 23:22:29 -0500

    Ejercicios Prueba Laboratorio

---
+++ Conceptos/PruebaLaboratorio/Variables.js
@@ -0,0 +1,13 @@
+// --- Ejemplo con let ---
+let edad = 25; // Declara una variable llamada 'edad' con el valor 25.
+console.log(edad); // Muestra: 25
+
+edad = 26; // ¡El valor puede ser reasignado!
+console.log(edad); // Muestra: 26
+
+// --- Ejemplo con const ---
+const nombre = "Ana"; // Declara una constante llamada 'nombre' con el valor "Ana".
+console.log(nombre); // Muestra: "Ana"
+
+// nombre = "Juan"; // Si intentas hacer esto, obtendrás un error.
+// TypeError: Assignment to constant variable.
